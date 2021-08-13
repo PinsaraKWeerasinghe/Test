dcf372650cb03fd146fa40655da639d58e303481
PinsaraW <w.pinsara@theglobalmarketaccess.com>
2021-08-13 18:48:36 +0530

    test

---
+++ src/main/java/test/pnx/userprofile/Test2.java
@@ -0,0 +1,85 @@
+package test.pnx.userprofile;
+
+public class Test2 {
+    public static void main(String[] args) {
+        String dtring=":old.company_id,\n" +
+                "                :old.parent_company_id,\n" +
+                "                :old.company_code,\n" +
+                "                :old.isin_code,\n" +
+                "                :old.established_date,\n" +
+                "                :old.incorporated_date,\n" +
+                "                :old.country_code,\n" +
+                "                :old.phone_1,\n" +
+                "                :old.phone_2,\n" +
+                "                :old.phone_3,\n" +
+                "                :old.fax_1,\n" +
+                "                :old.fax_2,\n" +
+                "                :old.web,\n" +
+                "                :old.email,\n" +
+                "                :old.eps,\n" +
+                "                :old.last_updated_time,\n" +
+                "                :old.industry_code,\n" +
+                "                :old.is_research_provider,\n" +
+                "                :old.trade_currency,\n" +
+                "                :old.state,\n" +
+                "                :old.fund_related,\n" +
+                "                :old.company_type_id,\n" +
+                "                :old.fy_start,\n" +
+                "                :old.sharia_compliance,\n" +
+                "                :old.sharia_board,\n" +
+                "                :old.financial_template_group_id,\n" +
+                "                :old.contact_person,\n" +
+                "                :old.profile_status,\n" +
+                "                :old.logo,\n" +
+                "                :old.city_id,\n" +
+                "                :old.fy_start_mon,\n" +
+                "                :old.template_id,\n" +
+                "                :old.ms_industry_group_id,\n" +
+                "                :old.listing_status,\n" +
+                "                :old.authorized_capital,\n" +
+                "                :old.financial_stmnt_freq,\n" +
+                "                :old.dividend_payment_freq,\n" +
+                "                :old.paid_capital,\n" +
+                "                :old.linkedin,\n" +
+                "                :old.twitter,\n" +
+                "                :old.youtube,\n" +
+                "                :old.facebook,\n" +
+                "                :old.sla_level,\n" +
+                "                :old.market_cap,\n" +
+                "                :old.market_cap_upd_time,\n" +
+                "                :old.id_bb_global_company,\n" +
+                "                :old.id_bb_global,\n" +
+                "                :old.authorised_capital_currency,\n" +
+                "                :old.paid_capital_currency,\n" +
+                "                :old.last_reviewed_date,\n" +
+                "                :old.last_reviewed_date_ref,\n" +
+                "                :old.enable_ir_url,\n" +
+                "                :old.date_of_registration,\n" +
+                "                :old.country_of_registration,\n" +
+                "                :old.city_of_registration,\n" +
+                "                :old.chamber_of_com_registered,\n" +
+                "                :old.registration_number,\n" +
+                "                :old.license_number,\n" +
+                "                :old.license_expiry_date,\n" +
+                "                :old.legal_form_id,\n" +
+                "                :old.free_zone_id,\n" +
+                "                :old.date_of_registration_ref,\n" +
+                "                :old.license_expiry_date_ref,\n" +
+                "                :old.company_completeness_value,\n" +
+                "                :old.company_completeness_rank,\n" +
+                "                :old.is_qualified,\n" +
+                "                :old.mdss,\n" +
+                "                :old.mobile,\n" +
+                "                :old.banking_relationship,\n" +
+                "                :old.regulator,\n" +
+                "                :old.is_ir_research_provider,\n" +
+                "                :old.rating_snp,\n" +
+                "                :old.rating_moody,\n" +
+                "                :old.rating_fitch,\n" +
+                "                :old.created_by,\n" +
+                "                :old.modified_by,\n" +
+                "                :old.logo_last_updated_time";
+
+        System.out.println(dtring.split(",").length);
+    }
+}
